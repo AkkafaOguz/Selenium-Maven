92cb66aaf7ddf0aef4e2016e7bab553f8161201b
Oguzhan <akkafaoguz@gmail.com>
2021-10-09 11:58:33 +0300

    odev

--- src/test/java/day05/Odev.java
+++ src/test/java/day05/Odev.java
@@ -1,5 +1,16 @@
 package day05;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Odev {
 
 //1-Test01 isimli bir class olusturun
@@ -12,4 +23,76 @@ public class Odev {
 //8-”Nutella” icin arama yapiniz
 //9- Kac sonuc bulundugunu yaziniz
 //10-Sayfayi kapatin
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test01()  {
+
+        driver.get("https://www.amazon.com/");
+        driver.navigate().refresh();
+
+    }
+
+    //--> @Test'ler kendi aralarinda random olarak calistiklari icin titleTest() method'u test01() methodundan once calisiyor. Bu yuzden 51. satirda getTitle() methodu hiclik donduruyor.
+    //--> Bunun onune gecmek icin en sagliklisi hepsini ayni method icerisinde yazmak. (Fakat boyle birakiyorum ki hatami daha sonra gorebileyim)
+
+    @Test
+    public void titleTest() {
+
+        String expectedTitlePart = "Amazon";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.contains(expectedTitlePart)) {
+            System.out.println("Test for 'Expected Title Part' is pass!");
+        } else {
+            System.out.println("Test for 'Expected Title Part' is failed!");
+            System.out.println("The actual title is: " + actualTitle);
+        }
+
+        String expectedTitle = "Amazon.com. Spend less. Smile more.";
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Test for 'Ecpected Title' is pass!");
+        } else {
+            System.out.println("Test for 'Ecpected Title' is failed!");
+            System.out.println("The actual title is: " + actualTitle);
+        }
+
+    }
+
+    @Test
+    public void urlTest() {
+
+        String expectedURLPart = "amazon.com";
+        String actualURL = driver.getCurrentUrl();
+
+        if (actualURL.contains(expectedURLPart)) {
+            System.out.println("Test for 'Expected URL Part' is pass!");
+        } else {
+            System.out.println("Test for 'Expected URL Part' is failed!");
+            System.out.println("The actual URL is: " + actualURL);
+        }
+    }
+
+    @Test
+    public void searchTest() {
+
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nutella" + Keys.ENTER);
+        String numberOfResults = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']")).getText();
+    }
+
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
 }
